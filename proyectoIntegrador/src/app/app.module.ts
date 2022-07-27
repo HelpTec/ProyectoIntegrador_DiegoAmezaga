da9a6b0abe99ad70a779cc123079c355d89a9f85
HelpTec <diegoamezaga@gmail.com>
2022-07-27 11:51:22 -0300

    header, links y banner

--- proyectoIntegrador/src/app/app.module.ts
+++ proyectoIntegrador/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { RsocialesComponent } from './components/rsociales/rsociales.component';
+import { BannerComponent } from './components/banner/banner.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    RsocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
