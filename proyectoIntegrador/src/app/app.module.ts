2adbba83d2fe90009a218146c0c57d1e0a32957a
HelpTec <diegoamezaga@gmail.com>
2022-07-28 00:11:57 -0300

    agrego about, experiencias, estudios y comienzo de skills con p bar

--- proyectoIntegrador/src/app/app.module.ts
+++ proyectoIntegrador/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { RsocialesComponent } from './components/rsociales/rsociales.component';
 import { BannerComponent } from './components/banner/banner.component';
-
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,16 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoApComponent,
     RsocialesComponent,
-    BannerComponent
+    BannerComponent,
+    AboutComponent,
+    ExperienciasComponent,
+    EstudiosComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
