c698436075177b7d685729fd207efdc71ced68de
HelpTec <diegoamezaga@gmail.com>
2022-08-24 21:11:42 -0300

    Update AuthController.java

--- proyectoIntegrador/BackEnd/src/main/java/com/portfolio/proyectofinalbcknd/Security/Controller/AuthController.java
+++ proyectoIntegrador/BackEnd/src/main/java/com/portfolio/proyectofinalbcknd/Security/Controller/AuthController.java
@@ -60,9 +60,7 @@ public class AuthController {
         if(nuevoUsuario.getRoles().contains("admin")){
             roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
         }
-        
         usuario.setRoles(roles);
-        
         usuarioService.save(usuario);
         
         return new ResponseEntity(new Mensaje("Usuario guardado"),HttpStatus.CREATED);
